30fe06f189bd60238b8fa1cbab48985563e928e8
JuanPabdl <A00825316@exatec.tec.mx>
2025-12-16 16:34:46 -0600

    style (Footer): spacing and responsiveness adjustments

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,12 +1,12 @@
 const Footer = () => {
     return(
         <footer className="w-full bg-orange-500 text-white text-center">
-            <div className="flex mx-auto justify-between items-center p-6">
+            <div className="flex flex-col md:flex-row mx-auto gap-4 justify-between items-center py-10 px-4">
                 <p className="text-3xl font-bold tracking-tight">
                     Mozzirella's Pizza
                 </p>
-                <p>&copy; 2024 My Company. All rights reserved.</p>
-                <div className="flex gap-3 text-lg">
+                <p className="text-white/50">&copy; 2025 My Company. All rights reserved.</p>
+                <div className="flex flex-col md:flex-row gap-3 text-lg tracking-tight">
                     <p>Terms of service</p>
                     <p>Privacy Policy</p>
                 </div>
