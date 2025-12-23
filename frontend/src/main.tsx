f9c7110656f598b557220e111146abc8636571ec
JuanPabdl <A00825316@exatec.tec.mx>
2025-12-23 17:16:00 -0600

    feat: create user request implemented

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Auth0ProviderWithNavigate>
-      <App />
-    </Auth0ProviderWithNavigate>
+    <QueryClientProvider client={queryClient}>
+      <Auth0ProviderWithNavigate>
+        <App />
+      </Auth0ProviderWithNavigate>
+    </QueryClientProvider>
   </StrictMode>,
 )
