5f94809fd9aecd5d4a2e0f96e5e231ed065962c1
JuanPabdl <A00825316@exatec.tec.mx>
2025-12-29 17:39:08 -0600

    fix: moved Auth0ProviderWN to router component

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "@/pages/Layout";
 import HomePage from "@/pages/HomePage";
+import Auth0ProviderWithNavigate from "@/auth/Auth0ProviderWithNavigate";
 
 const router = createBrowserRouter([
     {
         path: '/',
         errorElement: <div>Error, try again</div>,
-        element: <Layout/>,
+        element: (
+            <Auth0ProviderWithNavigate>
+                <Layout/>
+            </Auth0ProviderWithNavigate>
+        ),
         children: [
             {index: true, element: <HomePage/>},
             {path: '/menu', element: <div>Menu Page</div>},
