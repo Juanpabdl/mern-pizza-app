3fdf4e5b10b5866a6b713d277560143265e96547
JuanPabdl <A00825316@exatec.tec.mx>
2025-12-29 17:39:08 -0600

    fix: moved Auth0ProviderWN to router component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
 import MyRoutes from './router/router.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Auth0ProviderWithNavigate>
       <MyRoutes />
-    </Auth0ProviderWithNavigate>
   </StrictMode>,
 )
