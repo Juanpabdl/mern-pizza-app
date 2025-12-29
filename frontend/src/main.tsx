1031c0f78211716f2cefdf8a7266b562a0c7c2ad
JuanPabdl <A00825316@exatec.tec.mx>
2025-12-29 17:13:37 -0600

    Revert "feat: create user request implemented"
    This reverts commit 9004b23cd89ab20abc953880a00ec0d19d3b31dc.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,22 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Auth0ProviderWithNavigate>
-        <App />
-      </Auth0ProviderWithNavigate>
-    </QueryClientProvider>
+    <Auth0ProviderWithNavigate>
+      <App />
+    </Auth0ProviderWithNavigate>
   </StrictMode>,
 )
