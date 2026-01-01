f3a8c94afa28f7f4e3763eeb2d6f7fe9d2c95394
JuanPabdl <A00825316@exatec.tec.mx>
2026-01-01 15:10:11 -0600

    fix: backend Port const

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ mongoose.connect(process.env.DB_CONNECTION_STRING as string || "", {})
     console.error("Error connecting to database:", err);});
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
