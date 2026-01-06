8a55e93b82be5f7aea4ae52c044105e9d8beeea9
JuanPabdl <A00825316@exatec.tec.mx>
2026-01-06 16:20:28 -0600

    feat: update user route

--- backend/src/routes/myUserRoute.ts
+++ backend/src/routes/myUserRoute.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import myUserController from "../controller/myUserController.js";
-import { jwtCheck } from "../middleware/auth.js";
+import { jwtCheck, jwtParse } from "../middleware/auth.js";
+import { validateMyUserRequest } from "../middleware/validation.js";
 
 const router = express.Router();
 
 // api/my/user
 router.post("/", jwtCheck, myUserController.createUser);
+
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, myUserController.updateUser);
 
 export default router;
