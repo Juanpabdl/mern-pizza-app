3bda1b2312d4f237aaeab27fbf7f09a4c88fa9b1
JuanPabdl <A00825316@exatec.tec.mx>
2026-01-07 16:45:32 -0600

    update: hide Hero section for specific routes

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,13 +1,18 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+
 const Layout = () => {
+    const location = useLocation();
+    const hideHeroRoutes = ['/user-profile']
+
+    const hideHero = hideHeroRoutes.includes(location.pathname)
 
     return(
         <div className="flex flex-col min-h-screen w-screen">
             <Header/>
-            <Hero/>
+            {!hideHero && <Hero/>}
             <div className="mx-auto flex-1 py-10">
                 {/* Components go here */}
                 <Outlet/>
