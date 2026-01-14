6a96addf5acc38e8188ecd092758813ae76d6641
JuanPabdl <A00825316@exatec.tec.mx>
2026-01-14 17:57:58 -0600

    feat: useGetMyUser hook

--- frontend/src/api/myUserAPI.tsx
+++ frontend/src/api/myUserAPI.tsx
@@ -1,6 +1,7 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useAuth0 } from "@auth0/auth0-react";
 import { toast } from "sonner";
+import { type User } from "../types";
 
 const API_BASE_URL = import.meta.env.VITE_SERVER_URL;
 
@@ -96,5 +97,42 @@ export const useUpdateMyUser = () => {
     isPending,
     error,
     reset
+  };
+};
+
+export const useGetMyUser = () => {
+  const {getAccessTokenSilently} = useAuth0();
+
+  const getMyUserRequest = async (): Promise<User> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch user data");
+    }
+
+    return response.json();
+  }
+
+  const {
+    data: currentUser, 
+    isPending, 
+    error
+  } = useQuery({ queryKey: ["fetchCurrentUser"], queryFn: getMyUserRequest });
+
+  if(error) {
+    toast.error(error.toString());
+  }
+
+  return {
+    currentUser,
+    isPending,
   };
 };
