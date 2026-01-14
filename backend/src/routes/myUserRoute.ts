e88613cfefb5ca74f386925333a9a702ef107d48
JuanPabdl <A00825316@exatec.tec.mx>
2026-01-14 17:39:19 -0600

    feat: get user route functions

--- backend/src/routes/myUserRoute.ts
+++ backend/src/routes/myUserRoute.ts
@@ -6,6 +6,8 @@ import { validateMyUserRequest } from "../middleware/validation.js";
 const router = express.Router();
 
 // api/my/user
+router.get("/", jwtCheck, jwtParse, myUserController.getUser);
+
 router.post("/", jwtCheck, myUserController.createUser);
 
 router.put("/", jwtCheck, jwtParse, validateMyUserRequest, myUserController.updateUser);
