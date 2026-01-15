bdedabe03535584fc36757af9df2fbf18c84fbdf
JuanPabdl <A00825316@exatec.tec.mx>
2026-01-15 16:41:42 -0600

    feat: ProtectedRoute Component

---
+++ frontend/src/auth/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Outlet, Navigate } from "react-router";
+
+const ProtectedRoute = () => {
+    const { isAuthenticated } = useAuth0();
+
+    return isAuthenticated ? (
+        <Outlet />
+    ) : (
+        <Navigate to='/' replace/>
+    );
+}
+
+export default ProtectedRoute;
