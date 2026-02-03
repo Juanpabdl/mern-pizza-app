b8e57a710fdf08f55c8d7a53871f335f873da3f6
JuanPabdl <A00825316@exatec.tec.mx>
2026-02-02 18:05:51 -0600

    fix: replaced form component for field on userform

--- frontend/src/forms/user-profile-forms/UserProfileForm.tsx
+++ frontend/src/forms/user-profile-forms/UserProfileForm.tsx
@@ -1,12 +1,15 @@
 import {z} from "zod";
-import {useForm} from "react-hook-form";
+import {Controller, useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import LoadingButton from "@/components/LoadingButton";
 import type { User } from "@/types";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { Field, 
+        FieldError, 
+        FieldGroup, 
+        FieldLabel } from "@/components/ui/field";
 
 const formSchema = z.object({
     email: z.email().optional(),
@@ -25,101 +28,92 @@ type Props = {
 };
 
 const UserProfileForm = ({onSubmit, isLoading, currentUser}:Props) => {
-    const [formKey, setFormKey] = useState<number>(0);
     const form = useForm<UserFormData>({
         resolver: zodResolver(formSchema),
-        defaultValues: currentUser || { username: '', city: '', country: '', addressLine: '' }, //currentUser
+        defaultValues: currentUser || { username: '', city: '', country: '', addressLine: '' },
     });
 
     useEffect(() => {
-        if(currentUser){
-            //form.reset(); //form.reset(currentUser);
-            setFormKey(prevKey => prevKey + 1);
-        }
-    }, [currentUser]);
+        form.reset(currentUser);
+    }, [currentUser, form]);
 
-    return(
-        <Form key={formKey} {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} 
-            className="space-y-4 bg-gray-50 rounded-lg p-5 md:p-10">
-                <div className="space-y-1 mb-4">
-                    <h2 className="text-xl font-bold">User Profile Form</h2>
-                    <FormDescription>
-                        View and change your profile info. here!
-                    </FormDescription>
-                </div>
-                <FormField control={form.control} 
-                name="email" 
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>Email</FormLabel>
-                        <FormControl>
-                            <Input {...field} disabled className="bg-white"/>
-                        </FormControl>
-                    </FormItem>
+   return(
+    <form id={`user-profile-form-${currentUser?._id}`} {...form} onSubmit={form.handleSubmit(onSubmit)}
+    className="w-full max-w-[700px] mx-auto space-y-4 bg-gray-50 rounded-lg p-5 md:p-10">
+        <FieldGroup>
+            <Controller 
+            name="email"
+            control={form.control}
+            render={({field, fieldState}) => (
+                <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel>Email</FieldLabel>
+                    <Input {...field} id={field.name} disabled className="bg-white"/>
+                </Field>
+            )}
+            />
+            <Controller 
+            name="username"
+            control={form.control}
+            render={({field, fieldState}) => (
+                <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel>User Name</FieldLabel>
+                    <Input {...field} id={field.name} className="bg-white" placeholder="User Name"/>
+                    {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                </Field>
+            )}
+            />
+            <div className="flex flex-col sm:flex-row gap-4">
+                <Controller 
+                name="addressLine"
+                control={form.control}
+                render={({field, fieldState}) => (
+                    <Field className="flex-1" data-invalid={fieldState.invalid}>
+                        <FieldLabel>Address Line</FieldLabel>
+                        <Input {...field} id={field.name} className="bg-white" placeholder="Address Line"/>
+                        {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
                 )}
                 />
-                <FormField control={form.control} 
-                name="username" 
-                render={({ field }) => (
-                    <FormItem>
-                        <FormLabel>User Name</FormLabel>
-                        <FormControl>
-                            <Input {...field} className="bg-white"/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
+                <Controller 
+                name="country"
+                control={form.control}
+                render={({field, fieldState}) => (
+                    <Field className="flex-1" data-invalid={fieldState.invalid}>
+                        <FieldLabel>Country</FieldLabel>
+                        <Input {...field} id={field.name} className="bg-white" placeholder="Country"/>
+                        {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
+                )}
+                />  
+                <Controller
+                name="city"
+                control={form.control}
+                render={({field, fieldState}) => (
+                    <Field className="flex-1" data-invalid={fieldState.invalid}>
+                        <FieldLabel>City</FieldLabel>
+                        <Input {...field} className="bg-white" placeholder="City"/>
+                        {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                    </Field>
                 )}
                 />
-                {/* Address Fields */}
-                <div className="flex flex-col md:flex-row gap-4">
-                    <FormField control={form.control} 
-                    name="addressLine" 
-                    render={({ field }) => (
-                        <FormItem className="flex-1">
-                            <FormLabel>Address Line</FormLabel>
-                            <FormControl>
-                                <Input {...field} className="bg-white"/>
-                            </FormControl>
-                            <FormMessage/>
-                        </FormItem>
+            </div>
+            <div>
+                <Field orientation="horizontal">
+                    {/*<Button variant="outline" onClick={() => form.reset(currentUser)}>
+                        Reset
+                    </Button>*/}
+                    {isLoading ? (
+                        <LoadingButton />
+                    ) : (
+                        <Button type="submit" className="bg-orange-500">
+                            Submit
+                        </Button>
                     )}
-                    />
-                    <FormField control={form.control} 
-                    name="country" 
-                    render={({ field }) => (
-                        <FormItem className="flex-1">
-                            <FormLabel>Country</FormLabel>
-                            <FormControl>
-                                <Input {...field} className="bg-white"/>
-                            </FormControl>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                    />
-                    <FormField control={form.control} 
-                    name="city" 
-                    render={({ field }) => (
-                        <FormItem className="flex-1">
-                            <FormLabel>City</FormLabel>
-                            <FormControl>
-                                <Input {...field} className="bg-white"/>
-                            </FormControl>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                    />
-                </div>
-                {isLoading ? (
-                    <LoadingButton />
-                ) : (
-                    <Button type="submit" className="bg-orange-500">
-                        Submit
-                    </Button>
-                )}
-            </form>
-        </Form>
-    )
+                </Field>
+            </div>
+        </FieldGroup>
+    </form>
+   )
 }
 
 export default UserProfileForm;
