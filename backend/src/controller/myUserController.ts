6a61845159c01d751bc8e5814d3933f29c48812a
JuanPabdl <A00825316@exatec.tec.mx>
2026-02-09 18:17:15 -0600

    fix: User profile data displays

--- backend/src/controller/myUserController.ts
+++ backend/src/controller/myUserController.ts
@@ -10,7 +10,7 @@ const getUser = async (req: Request, res: Response) => {
             return res.status(404).json({ message: "User not found" });
         }
         //2. Respond with user data
-        res.status(200).json({ currentUser });
+        res.status(200).json(currentUser);
     } catch (error) {
         console.error("Error fetching user:", error);
         res.status(500).json({ message: "Internal server error", error });
