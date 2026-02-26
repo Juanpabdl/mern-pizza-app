48a8071f51f2bb82b7cd8b29943a17b1f9feacfd
JuanPabdl <A00825316@exatec.tec.mx>
2026-02-26 17:53:18 -0600

    feat: ManageMenuList & Item components

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,5 +1,6 @@
 import RestaurantMenuForm from "@/forms/manage-menu-form/RestaurantMenuForm"
 import { useCreateMyMenu, useGetMyMenu } from "@/api/myMenuAPI";
+import ManageMenuList from "@/components/ManageMenuList";
 
 const ManageRestaurantPage = () => {
     const  {createMenu, isPending: isCreateLoading} = useCreateMyMenu();
@@ -13,7 +14,13 @@ const ManageRestaurantPage = () => {
         return <div>No menu items found. Please add some dishes to your menu.</div>
     }
     
-    return <RestaurantMenuForm onSave={createMenu} isLoading={isCreateLoading} menuItem={menuItems[0]} />;
+    return (
+        <>
+            <RestaurantMenuForm onSave={createMenu} isLoading={isCreateLoading} menuItem={menuItems[0]} />
+            <ManageMenuList menuItems={menuItems} />
+        </>
+    
+);
 }
 
 export default ManageRestaurantPage;
