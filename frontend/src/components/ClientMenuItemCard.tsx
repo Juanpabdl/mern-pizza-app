b3752dff9c920cfdb4d3fe55b713e06be8262292
JuanPabdl <A00825316@exatec.tec.mx>
2026-04-06 17:19:49 -0600

    refact(ClientMenuCard): Card component added & implemented

--- frontend/src/components/ClientMenuItemCard.tsx
+++ frontend/src/components/ClientMenuItemCard.tsx
@@ -1,38 +1,51 @@
 import type { MenuItem } from "@/types";
 import { Button } from "./ui/button";
 import { Cross } from "lucide-react";
+import { useGetMenuItem } from "@/api/myMenuAPI";
+import { Card, CardDescription, CardFooter, CardTitle } from "./ui/card";
 
 type Props = {
   item: MenuItem;
 };
 
 const ClientMenuItemCard = ({ item }: Props) => {
+  const  {menuItem} = useGetMenuItem(item._id);
+
+  const addToCart = () => {
+    // Implement add to cart functionality here
+    console.log(`Adding ${menuItem?.name || item.name} to cart`);
+  };
+
   return (
-    <div className="border rounded-lg shadow-sm hover:shadow-md transition-shadow">
+    <Card className="border rounded-lg shadow-sm hover:shadow-md transition-shadow p-0">
       {item.imageUrl && (
         <img
-          src={item.imageUrl}
-          alt={item.name}
-          className="h-40 w-full object-cover rounded-t-md mb-3"
-        />
+        src={item.imageUrl}
+        alt={item.name}
+        className="max-h-40 w-full aspect-square object-cover rounded-t-md mb-1 mt-0"/>
       )}
-        <div className="px-4 pb-4">
-            <h3 className="text-md font-semibold mb-1">{item.name}</h3>
+        <div className="px-4 pb-4 space-y-2">
+          <CardTitle>
+            {item.name}
+          </CardTitle>
+          <CardDescription>
             <p className="text-sm text-gray-600">${item.price.toFixed(2)}</p>
             <p className="text-xs text-gray-500 mt-2">
                 Categories: {item.category.join(", ")}
             </p>
-            <div className="flex justify-end">
-                <Button 
-                variant="outline" 
-                size="sm" 
-                className="mt-4 bg-orange-500 text-white hover:bg-orange-600 rounded-full">
-                    <Cross className="h-5 w-5"/>
-                    Agregar
-                </Button>
-            </div>
+          </CardDescription>
         </div>
-    </div>
+        <CardFooter className="w-full flex justify-end align-middle pb-3 px-3">
+            <Button 
+            variant="outline" 
+            size="sm" 
+            className="mt-4 bg-orange-500 text-white hover:bg-orange-600 rounded-full"
+            onClick={addToCart}>
+                <Cross className="h-5 w-5"/>
+                Agregar
+            </Button>
+        </CardFooter>
+    </Card>
   );
 };
 
