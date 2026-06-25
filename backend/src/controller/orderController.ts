11e2a379d87697b2b11b88f66428f6d399e30623
JuanPabdl <A00825316@exatec.tec.mx>
2026-06-25 16:50:19 -0600

    feat: Order status update

--- backend/src/controller/orderController.ts
+++ backend/src/controller/orderController.ts
@@ -143,8 +143,30 @@ const createSession = async (lineItems: any, orderId: string) => {
     return session;
 }
 
+const updateOrderStatus = async (req: Request, res: Response) => {
+    try {
+        const { orderId } = req.params;
+        const { status } = req.body;
+
+        const order = await Order.findById(orderId);
+
+        if (!order) {
+            return res.status(404).json({ message: "Order not found" });
+        }
+
+        order.status = status;
+        await order.save();
+
+        res.status(200).json({ message: "Order status updated successfully", order});
+    } catch (error: any) {
+        console.error("Error updating order status:", error);
+        res.status(500).json({ message: "Unable to update order status"});
+    }
+}
+
 export default {
     createCheckoutSession,
     stripeWebhookHandler,
     getMyOrders,
+    updateOrderStatus,
 };
